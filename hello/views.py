227049e1cea68a4d59f6cdd3b5a20212a69dfc7e
Alan Jui <alanjui.1960@gmail.com>
2022-06-09 19:08:51 +0800

    完成「登錄訊息」功能；「顯示最新訊息」功能。

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,46 @@
 # import re
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.utils.timezone import datetime
+from django.views.generic import ListView
+
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+
+# from hello.models import LogMessage
 
 
-def home(request):
-    return render(request, 'hello/home.html')
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        # context = super(HomeListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+# def home(request):
+#     return render(request, 'hello/home.html')
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect('home')
+    else:
+        return render(
+            request,
+            'hello/log_message.html',
+            {
+                'form': form,
+            }
+        )
 
 
 def about(request):
